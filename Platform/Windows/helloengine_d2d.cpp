8a2b1e46dfe08b92694f4eeb10778026068a593a
FlashPanda <caizhoudong@126.com>
2019-01-15 21:26:18 +0800

    继续添加d2d代码

--- Platform/Windows/helloengine_d2d.cpp
+++ Platform/Windows/helloengine_d2d.cpp
@@ -16,3 +16,49 @@ inline void SafeRelease(T** ppInterfaceToRelease) {
 		(*ppInterfaceToRelease) = nullptr;
 	}
 }
+
+HRESULT CreateGraphicsResources(HWND hWnd) {
+	HRESULT hr = S_OK;
+	if (pRenderTarget == nullptr) {
+		RECT rc;
+		GetClientRect(hWnd, &rc);
+		
+		D2D1_SIZE_U size = D2D1::SizeU(rc.right - rc.left,
+										rc.bottom - rc.top);
+										
+		hr = pFactory ->CreateHwndRenderTarget(
+			D2D1::RenderTargetProperties(),
+			D2D1::HwndRenderTargetProperties(hWnd, size),
+			&pRenderTarget);
+			
+		if (SUCCEEDED(hr)) {
+			hr = pRenderTarget->CreateSolidColorBrush(D2D1::ColorF(D2D1::ColorF::LightSlateGray), &pLightSlateGrayBrush);
+		}
+		
+		if (SUCCEEDED(hr)) {
+			hr = pRenderTarget->CreateSolidColorBrush(D2D1::ColorF(D2D1::ColorF:CornflowerBlue), &pCornflowerBlueBrush);
+		}
+	}
+	
+	return hr;
+}
+
+void DiscardGraphicsResources() {
+	SafeRelease(&pRenderTarget);
+	SafeRelease(&pLightSlateGrayBrush);
+	SafeRelease(&pCornflowerBlueBrush);
+}
+
+// WindowProc 函数原型
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain(
+    HINSTANCE /* hInstance */,
+    HINSTANCE /* hPrevInstance */,
+    LPSTR /* lpCmdLine */,
+    int /* nCmdShow */
+    )
+{
+	HRESULT hr;
+	
+}
