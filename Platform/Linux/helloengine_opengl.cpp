1c1137566a652a295c04ff26f3e734a542a13bae
CompanyUbuntu <caizhoudong001@gmail.com>
2019-01-28 17:47:24 +0800

    opengl的实现

--- Platform/Linux/helloengine_opengl.cpp
+++ Platform/Linux/helloengine_opengl.cpp
@@ -50,7 +50,7 @@ static int cxtErrorHandler (Display* dpy, XErrorEvent* ev) {
 
 void DrawAQuad() {
   glClearColor(1.0, 1.0, 1.0, 1.0);
-  glClear(GL_COLOR_BUFFER_BIT | GL_DETH_BUFFER_BIT);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
@@ -81,7 +81,7 @@ int main (void) {
   xcb_generic_event_t*    pEvent;
   xcb_colormap_t colormap;
   uint32_t                mask = 0;
-  uint32_t  values[3];
+  uint32_t                values[3];
   uint8_t                 isQuit = 0;
 
   char title[] = "Hello, Engine![OpenGL]";
@@ -136,67 +136,203 @@ int main (void) {
 
   default_screen = DefaultScreen(display);
 
+  // 查询帧缓存配置
+  fb_configs = glXChooseFBConfig(display, default_screen, visual_attribs, &num_fb_configs);
+  if (!fb_configs || num_fb_configs) {
+    fprintf(stderr, "glXGetFBConfigs failed\n");
+    return -1;
+  }
 
-  // 与xServer建立链接
-  pConn = xcb_connect(0, 0);
+  /* Pick the FB config/visual with the most samples per pixel */
+  {
+    int best_fbc = -1, worst_fbc = -1, best_num_samp = -1, worst_num_samp = 999;
 
-  // 获取第一个屏幕
-  pScreen = xcb_setup_roots_iterator(xcb_get_setup(pConn)).data;
+    for (int i = 0; i < num_fb_configs; ++i) {
+      XVisualInfo* vi = glXGetVisualFromFBConfig (display, fb_configs[i]);
+      if (vi) {
+        int samp_buf, samples;
+        glXGetFBConfigAttrib(display, fb_configs[i], GLX_SAMPLE_BUFFERS, &samp_buf);
+        glXGetFBConfigAttrib(display, fb_configs[i], GLX_SMAPLES, &samples);
 
-  // 获取根窗口
+        printf(" Matching fbcofnig %d, visual ID 0x%lx: SAMPLE_BUFFERS = %d,"
+               " SAMPLES = %d\n",
+               i, vi ->visualid, samp_buf, samples);
+
+        if (best_fbc < 0 || (samp_buf && samples > best_num_samp))
+          best_fbc = i, best_num_samp = samples;
+        if (worst_fbc < 0 || !samp_buf || samples < worst_num_samp)
+          worst_fbc = i, worst_num_samp = samples;
+      }
+      XFree(vi);
+    }
+
+    fb_config = fb_configs[best_fbc];
+  }
+
+  /* 获取一个显示 */
+  vi = glXGetVisualFromFBConfig (display, fb_config);
+  printf("Chosen visual ID = 0x%lx\n", vi->visualid);
+
+  /* 与xServer建立连接 */
+  pConn = XGetXCBConnection(display);
+  if (!pConn) {
+    XCloseDisplay(display);
+    fprintf(stderr, "Can't get xcb connection from display\n");
+    return -1;
+  }
+  
+  /* 获取事件队列包含关系 */
+  XSetEventQueueOwner(display, XCBOwnsEventQueue);
+
+  /* 找到XCB屏幕 */
+  xcb_screen_iterator_t screen_iter = 
+    xcb_setup_roots_iterator(xcb_get_setup(pConn));
+  for (int screen_num = vi ->screen; screen_iter.rem && screen_num > 0;
+      --screen_num, xcb_screen_next(&screen_iter));
+  pScreen = screen_iter.data;
+
+  /* 获取根窗口 */
   window = pScreen->root;
 
-  // 创建前端图形上下文
-  foreground = xcb_generate_id(pConn);
-  mask = XCB_GC_FOREGROUND | XCB_GC_GRAPHICS_EXPOSURES;
-  values[0] = pScreen->black_pixel;
-  values[1] = 0;
-  xcb_create_gc(pConn, foreground, window, mask, values);
+  /* 为colormap创建XID */
+  colormap = xcb_generate_id(pConn);
 
-  // 创建后端图形上下文
-  background = xcb_generate_id(pConn);
-  mask = XCB_GC_BACKGROUND | XCB_GC_GRAPHICS_EXPOSURES;
-  values[0] = pScreen->white_pixel;
-  values[1] = 0;
-  xcb_create_gc(pConn, background, window, mask, values);
+  xcb_create_colormap(
+      pConn,
+      XCB_COLORMAP_ALLOC_NONE,
+      colormap,
+      window,
+      vi->visualid);
 
-  // 创建窗口
+  /* 创建窗口 */
   window = xcb_generate_id(pConn);
-  mask = XCB_CW_BACK_PIXEL | XCB_CW_EVENT_MASK;
-  values[0] = pScreen->white_pixel;
-  values[1] = XCB_EVENT_MASK_EXPOSURE | XCB_EVENT_MASK_KEY_PRESS;
-  xcb_create_window(pConn, 
-                    XCB_COPY_FROM_PARENT,
-                    window,
-                    pScreen->root,
-                    20, 20,
-                    640, 400,
-                    10,
-                    XCB_WINDOW_CLASS_INPUT_OUTPUT,
-                    pScreen->root_visual,
-                    mask, values);
+  mask = XCB_CW_EVENT_MASK | XCB_CW_COLORMAP;
+  values[0] = XCB_EVENT_MASK_EXPOSURE | XCB_EVENT_MASK_KEY_PRESS;
+  values[1] = colormap;
+  values[2] = 0;
+  xcb_create_window(pConn, // 连接
+                    XCB_COPY_FROM_PARENT, // 深度缓存
+                    window, // 窗口ID
+                    pScreen->root,  // 父窗口
+                    20, 20, // x,y
+                    640, 480,// 宽，高
+                    10, // 边框宽度
+                    XCB_WINDOW_CLASS_INPUT_OUTPUT, // 类
+                    vi->visualid, // 显示
+                    mask, values);  // 掩码
+  XFree(vi);
 
-  // 设置窗口标题
+  /* 设置窗口的标题 */
   xcb_change_property(pConn, XCB_PROP_MODE_REPLACE, window,
-                      XCB_ATOM_WM_NAME, XCB_ATOM_STRING, 8,
-                      strlen(title), title);
+      XCB_ATOM_WM_NAME, XCB_ATOM_STRING, 8, strlen(title), title);
 
-  xcb_change_property(pConn, XCB_PROP_MODE_REPLACE, window, 
-                      XCB_ATOM_WM_ICON_NAME, XCB_ATOM_STRING, 8,
-                      strlen(title_icon), title_icon);
+  /* 设置窗口图标的标题 */
+  xcb_change_property(pConn, XCB_PROP_MODE_REPLACE, window,
+      XCB_ATOM_WM_ICON_NAME, XCB_ATOM_STRING, 8,
+      strlen(title_icon), title_icon);
 
-  // 映射窗口到屏幕上
+  /* 映射窗口到屏幕上 */
   xcb_map_window(pConn, window);
 
   xcb_flush(pConn);
 
-  while ((pEvent = xcb_wait_for_event(pConn)) && !isQuit) {
-    switch(pEvent->response_type & ~0x80) {
+  /* 获取默认屏幕的GLX扩展列表 */
+  glxExts = glXQueryExtensionsString(display, default_screen);
+
+  /* 注意：在调用glXGetProcAddressARB之桥创建当前上下文不是必要操作 */
+  glXCreateContextAttribsARB = (glXCreateContextAttribsARBProc)
+    glXGetProcAddressARB((const GLubyte *)"glXCreateContextAttribsARB");
+
+  /* 创建OpenGL上下文 */
+  ctxErrorOccurred = false;
+  int (*oldHandler)(Display*, XErrorEvent*) = XSetErrorHandler(&ctxErrorHandler);
+
+  if (!isExtensionSupperted(glxExts, "GLX_ARB_create_context") || 
+      !glXCreateContextAttribsARB) {
+    printf("glXCreateContextAttribsARB() not found"
+        "... using old-style GLX context\n");
+    context = glXCreateNewContext(display, fb_config, GLX_RGBA_TYPE, 0, True);
+    if (!context) {
+      fprintf(stderr, "glXCreateNewContext failed\n");
+      return -1;
+    }
+  }
+  else {
+    int context_attribs[] = {
+      GLX_CONTEXT_MAJOR_VERSION_ARB, 4,
+      GLX_CONTEXT_MINOR_VERSION_ARB, 0,
+      None
+    };
+
+    printf("Creating context\n");
+    context = glXCreateContextAttribsARB(display, fb_config, 0, True, context_attribs);
+
+    XSync(display, False);
+    if (!ctxErrorOccurred && context)
+      printf("Create GL 3.0 context \n");
+    else {
+      /* GLX_CONTEXT_MAJOR_VERSION_ARB = 1*/
+      context_attribs[1] = 1;
+      /* GLX_CONTEXT_MINOR_VERSION_ARB = 0*/
+      context_attribs[3] = 0;
+
+      ctxErrorOccurred = false;
+
+      printf("Failed to create GL 3.0 context"
+             "... using old-style GLX context\n");
+      context = glXCreateContextAttribsARB(display, fb_config, 0, True, context_attribs);
+    }
+  }
+
+  XSync(display, False);
+
+  XSetErrorHandler(oldHandler);
+
+  if (ctxErrorOccurred || !context) {
+    printf("Failed to create an OpenGL context\n");
+    return -1;
+  }
+
+  /* 确保那个上下文是一个直接上下文 */
+  if (!glXIsDirect(display, context)) {
+  printf("Indirect GLX rendering context obtained\n");
+  }
+  else {
+    printf("Direct GLX rendering context obtained\n");
+  }
+
+  /* 创建GLX窗口 */
+  glxwindow = glXCreateWindow(
+                display,
+                fb_config, 
+                window,
+                0);
+
+  if (!window) {
+    xcb_destroy_window(pConn, window);
+    glXDestroyContext(display, context);
+
+    fprintf(stderr, "glXDestroyContext failed\n");
+    return -1;
+  }
+
+  drawable = glxwindow;
+
+  /* 把OpenGL上下为设置成当前 */
+  if (!glXMakeContextCurrent (display, drawable, drawable, context))  {
+    xcb_destroy_window(pConn, window);
+    glXDestroyContext(display, context);
+
+    fprintf(stderr, "glXMakeContextCurrent failed\n");
+    return -1;
+  }
+
+  while (!isQuit && (pEvent = xcb_wait_for_event(pConn))) {
+    switch (pEvent->response_type & ~0x80) {
       case XCB_EXPOSE:
         {
-          xcb_rectangle_t rect = {20, 20, 60, 80};
-          xcb_poly_fill_rectangle(pConn, window, foreground, 1, &rect);
-          xcb_flush(pConn);
+          DrawAQuad();
+          glXSwapBuffers(display, drawable);
         }
         break;
       case XCB_KEY_PRESS:
@@ -206,6 +342,7 @@ int main (void) {
     free(pEvent);
   }
 
+  /* 清理 */
   xcb_disconnect(pConn);
 
   return 0;
