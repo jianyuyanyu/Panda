642b5005c69e3eb2439eff04b4d857a48fe056f8
Panda <caizhoudong001@gmail.com>
2019-04-03 21:45:43 +0800

    添加Allocator

--- Framework/Common/Allocator.cpp
+++ Framework/Common/Allocator.cpp
@@ -3,149 +3,91 @@
 #include <cstring>
 
 #ifndef ALIGN
-#define ALIGN(x, a)				(((x) + ((a) - 1)) & ~((a) - 1))
+#define ALIGN(n, a) (((n) + (a - 1)) & ~((a) - 1))		// 获取对齐的最小值
 #endif
 
-using namespace Panda;
-
-Panda::Allocator::Allocator(size_t data_size, size_t page_size, size_t alignment)
-	: m_pPageList(nullptr), m_pFreeList(nullptr)
+Panda::Allocator::Allocator(uint32_t inPageSize, uint32_t inBlockSize, uint32_t inAlignment) 
+	:m_pFreeBlockList(nullptr), m_pPageList(nullptr);
 {
-	Reset(data_size, page_size, alignment);
+	Reset(inPageSize, inBlockSize, inAlignment);
 }
 
-Panda::Allocator::~Allocator() 
+Panda::Allocator::~Allocator()
 {
 	FreeAll();
 }
 
-void Panda::Allocator::Reset(size_t data_size, size_t page_size, size_t alignment)
-{
-	FreeAll();
-	
-	m_szDataSize = data_size;
-	m_szPageSize = page_size；
-	
-	size_t minimal_size = (sizeof(BlockHeader) > m_szDataSize) ? sizeof (BlockHeader) : m_szDataSize;
-	
-	// this magic only works when alignment is 2^n, which should general be the case
-	// because most CPU/GPU also requres the alignment be in 2^name
-	// but still we use a assert to guarantee it
-#if defined(_DEBUG)
-	assert(alighment > 0 && ((alighment & (alignment - 1))) == 0);
-#endif
-	m_szBlockSize = ALIGN(minimal_size, alignment);	// block size must match the alignment
-	
-	m_szAlignmentSize = m_szBlockSize - minimal_size;
-	
-#if defined(_DEBUG)
-	assert(m_szPageSize - sizeof(PageHeader) - m_szBlockSize > 0);	// at least one block per page
-#endif
-	
-	m_nBlocksPerPage = (m_szPageSize - sizeof (PageHeader)) / m_szBlockSize;
-}
-
-// Allocate a new block
 void* Panda::Allocator::Allocate() {
-	if (!m_pFreeList) {
-		// allocate a new pate
-		PageHeader* pNewPage = reinterpret_cast<PageHeader*>(new uint8_t[m_szPageSize]);
-		++m_nPages;
-		m_nBlocks += m_nBlockPerPage;	// 总块数
-		m_nFreeBlocks += m_nBlockPerPage;	// 未使用块数
+	FreeAll();
+	
+	if (m_pFreeList == nullptr) {
+		// 分配一页内存
+		PageHeader* pNewPage = reinterpret_cast<PageHeader*>(new uint8_t[m_PageSize]);
+		m_FreeBlockCount += m_BlockCountPerPage;
+		m_BlockCount += m_BlockCountPerPage;
+		m_PageCount++;
 		
-#if defined (_DEBUG)
-		FillFreePage(pNewPage);
-#endif
-
-		if (m_pPageList) {
-			pNewPage->pNext = m_pPageList;
-		}
-		
+		pNewPage->pNext = m_pPageList;
 		m_pPageList = pNewPage;
-		
-		BlockHeader* pBlock = pNewPage->Blocks();
-		// link each block in the page
-		for (uint32_t i = 0; i < m_nBlockPerPage; ++i) {
-			pBlock->pNext = NextBlock(pBlock);
-			pBlock = pBlock->pNext;
+	 	
+		// 将所有内存块串联起来
+		BlockHeader* pBlockStart = m_pPageList->BlockStart();
+		m_pFreeBlockList = pBlockStart;
+		for (size_t i = 0; i < m_nBlockCountPerPage; ++i) {
+			pBlockStart->pNext = NextBlock(pBlockStart);
+			pBlockStart = pBlockStart->pNext;
 		}
-		pBlock->pNext = nullptr;
-		
-		m_pFreeList = pNewPage->Blocks();
+		pBlockStart->pNext = nullptr;
 	}
 	
-	BlockHeader* freeBlock = m_pFreeList;
-	m_pFreeList = m_pFreeList->pNext;
-	--m_nFreeBlocks;
+	// 取一块内存返回
+	BlockHeader* pBlock = m_pFreeBlockList;
+	m_pFreeBlockList = m_pFreeBlockList->pNext;
+	m_FreeBlockCount--;
 	
-#if defined (_DEBUG)
-	FillAllocatedBlock(freeBlock);
-#endif
-	return reinterpret_cast<void*>(freeBlock);
+	return reinterpret_cast<void*>(pBlock);
 }
 
-void Panda::Allocator::Free(void* p) {
-	BlockHeader* block = reinterpret_cast<BlockHeader*>(p);
-	
-#if defined(_DEBUG)
-	FillFreeBlock(block);
-#endif
-
-	block->pNext = m_pFreeList;
-	m_pFreeList = block;
-	++m_nFreeBlocks;
+Panda::BlockHeader* Panda::Allocator::NextBlock(BlockHeader* pCurrentBlock) {
+	return reinterpret_cast<BlockHeader*>( reinterpret_cast<uint8_t*> (pCurrentBlock) + m_nBlockSize);
 }
 
-void Panda::Allocator::FreeAll() {
-	PageHeader* pPage = m_pPageList;
-	while (pPage) {
-		PageHeader* _p = pPage;
-		pPage = pPage->pNext;
+// 重置分配器
+void Panda::Allocator::Reset(uint32_t inPageSize, uint32_t inBlockSize, uint32_t inAlignment) {
+	FreeAll();
+	
+	// 块的大小必须大于块头的大小，并且需要对齐
+	size_t minBlockSize = inBlockSize > sizeof(BlockHeader)? inBlockSize : sizeof(BlockHeader);
+	m_BlockSize = ALIGN(minBlockSize, inAlignment);
+	
+	m_AlignmentSize = m_BlockSize - minBlockSize;
+	
+	// 一页至少一个块
+	m_PageSize = inPageSize > (m_BlockSize + sizeof (PageHeader))? inPageSize : (m_BlockSize + sizeof(PageHeader));
+	
+	m_BlockCountPerPage = (m_PageSize - sizeof(PageHeader)) / m_BlockSize;
+	
+}
+
+void Panda::Allocator::Free(void* p) 
+{
+	BlockHeader* pHeader = reinterpret_cast<BlockHeader*>(p);
+	pHeader->pNext = m_pFreeBlockList;
+	m_pFreeBlockList = pHeader;
+	m_FreeBlockCount++;
+}
+
+void Panda::Allocator::FreeAll() 
+{
+	PageHeader* pFree = m_pPageList;
+	while (pFree) {
+		PageHeader* pTemp = pFree;
+		pFree = pFree->pNext;
 		
-		delete[] reinterpret_cast<uint8_t*>(_p);
+		delete[] reinterpret_cast<uint8_t*>(pTemp);
 	}
 	
+	// 只重置这两个属性因为这两个属性是要用于判断的，其他的都是保存作用
 	m_pPageList = nullptr;
-	m_pFreeList = nullptr;
-	
-	m_nPages	= 0;
-	m_nBlocks	= 0;
-	m_nFreeBlocks = 0;
-}
-
-#if defined (_DEBUG)
-void Panda::Allocator::FillFreePage(PageHeader* pPage) {
-	// page header
-	pPage->pNext = nullptr;
-	
-	// blocks
-	BlockHeader* pBlock = pPage->Blocks();
-	for (uint32_t i = 0; i < m_nBlockPerPage; ++i) {
-		FillFreeBlock(pBlock);
-		pBlock = NextBlock(pBlock);
-	}
-}
-
-void Panda::Allocator::FillFreeBlock(BlockHeader* pBlock) {
-	// block header + data
-	std::memset(pBlock, PATTERN_FREE, m_szBlockSize - m_szAlignmentSize);
-	
-	// alignment
-	std::memset(reinterpret_cast<uint8_t*>(pBlock) + m_szBlockSize - m_szAlignmentSize,
-				PATTERN_ALIGN, m_szAlignmentSize);
-}
-
-void Panda::Allocator::FillAllocatedBlock(BlockHeader* pBlock) {
-	// block header + data
-	std::memset(pBlock, PATTERN_ALLOC, m_szBlockSize - m_szAlignmentSize);
-	
-	// alignment
-	std::memset(reinterpret_cast<uint8_t*>(pBlock) + m_szBockSize - m_szAlignmentSize,
-				PATTERN_ALIGN, m_szAlignmentSize);
-}
-#endif
-
-Panda::BlockHeader* Panda::Allocator::NextBlock(BlockHeader* pBlock) {
-	return reinterpret_cast<BlockHeader*>(reinterpret_cast<uint8_t*>(pBlock) + m_szBlockSize);
+	m_pFreeBlockList = nullptr;
 }
