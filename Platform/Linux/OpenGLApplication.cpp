34106e358eec38344061bf7bf177d8913582a4ec
caizhoudong <caizhuodong001@gmail.com>
2019-04-15 14:20:35 +0800

    OpenGLApplication code continue

--- Platform/Linux/OpenGLApplication.cpp
+++ Platform/Linux/OpenGLApplication.cpp
@@ -106,7 +106,76 @@ int Panda::OpenGLApplication::Initailize()
         return -1;
     }
 
-    
+    // Pick the FB config/visual with the most samples per pixel
+    {
+        int bestFbc = -1, worstFbc, bestNumSamp = -1, worstNumSamp = 999;
+
+        for (int i = 0; i < numFbConfigs; ++i)
+        {
+            XVisualInfo* vi = glXGetVisualFromFBConfig(m_pDisplay, fbConfigs[i]);
+            if (vi)
+            {
+                int sampBuf, samples;
+                glXGetFBConfigAttrib(m_pDisplay, fbConfigs[i], GLX_SAMPLE_BUFFERS, &sampBuf);
+                glXGetFBConfigAttrib(m_pDisplay, fbConfigs[i], GLX_SAMPLES, &samples);
+
+                printf( " Matching fbconfig %d, visual ID 0x%lx: SAMPLE_BUFFERS = %d,"
+                        " SAMPLES = %d\n",
+                        i, vi->visualid, sampBuf, samples);
+
+                if (bestFbc < 0 || (sampBuf && samples > bestNumSamp))
+                    bestFbc = i, bestNumSamp = samples;
+                if (worstFbc < 0 || !sampBuf || samples < worstNumSamp)
+                    worstFbc = i, worstNumSamp = samples;
+            }
+            XFree(vi);
+        }
+
+        fbConfig = fbConfigs[bestFbc];
+    }
+
+    // Get a visual
+    vi = glXGetVisualFromFBConfig(m_pDisplay, fbConfig);
+    printf("Chosen visual ID = 0x%lx\n", vi->visualid);
+
+    // establish connection to X server
+    m_pConn = XGetXCBConnection(m_pDisplay);
+    if (!m_pConn)
+    {
+        XCloseDisplay(m_pDisplay);
+        fprintf(stderr, "Can't get xcb connection from display\n");
+        return -1;
+    }
+
+    // Acquire event queue ownership
+    XSetEventQueueOwner(m_pDisplay, XCBOwnsEventQueue);
+
+    // Find XCB screen
+    xcb_screen_iterator_t screenIter = xcb_setup_roots_iterator(xcb_get_setup(m_pConn));
+    for (int screenNum = vi->screen; screenIter.rem && screenNum > 0;
+        --screenNum, xcb_screen_next(&screenIter));
+    m_pScreen = screenIter.data;
+    m_Vi = vi->visualid;
+
+    result = XcbApplication::Initialize();
+    if (result)
+    {
+        printf("Xcb Application initialize fialed!");
+        return -1;
+    }
+
+    // Get the default screen's GLX extension list
+    glxExts = glXQueryExtensionsString(m_pDisplay, defaultScreen);
+
+    // Create OpenGL context
+    ctxErrorOccurred = false;
+    int (*oldHandler)(Display*, XErrorEvent*) = XSetErrorHandler(&cxtErrorHandler);
+
+    if (!isExtensionSupported(glxExts, "GLX_ARB_create_context") ||
+        !glXCreateContextAttribsARB)
+    {
+        print
+    }
 }
 
 void Panda::OpenGLApplication::Finalize()
