d172480f3f79616a765ef8d4fa37760dd2eb1c53
Panda <caizhoudong001@gmail.com>
2019-04-21 20:33:19 +0800

    add math library: vector

---
+++ Framework/Math/Vector2D.h
@@ -0,0 +1,88 @@
+#pragma once
+
+#include <math.h>
+
+/**
+ * A vector in 2D with components X, Y  
+ */
+
+struct Vector2D
+{
+public:
+    float x;
+    float y;
+
+    __forceinline Vector2D() {};
+    __forceinline Vector2D(const Vector2D& inV) : x(inV.x), y(inV.y) {}
+    __forceinline Vector2D(float _v): x(_v), y(_v) {}
+    __forceinline Vector2D(float _x, float _y) : x(_x), y(_y) {}
+
+    __forceinline Vector2D& operator= (const Vector2D& inV) 
+    {
+        x = inV.x;
+        y = inV.y;
+        return *this;
+    }
+
+    __forceinline Vector2D& operator+= (const Vector2D& inV)
+    {
+        x += inV.x;
+        y += inV.y;
+        return *this;
+    }
+
+    __forceinline Vector2D& operator-= (const Vector2D& inV)
+    {
+        x -= inV.x;
+        y -= inV.y;
+        return *this;
+    }
+
+    __forceinline Vector2D operator+(const Vector2D& inV)
+    {
+        return Vector2D(x + inV.x, y + inV.y);
+    }
+
+    __forceinline Vector2D operator- (const Vector2D& inV)
+    {
+        return Vector2D(x - inV.x, y - inV.y);
+    }
+
+    __forceinline Vector2D operator+(float bias)
+    {
+        return Vector2D(x + bias, y + bias);
+    }
+
+    __forceinline Vector2D operator-(float bias)
+    {
+        return Vector2D(x - bias, y - bias);
+    }
+
+    __forceinline Vector2D operator*(float scale)
+    {
+        return Vector2D(x * scale, y * scale);
+    }
+
+    __forceinline Vector2D operator/(float scale)
+    {
+        const float rScale = 1.f / scale;
+        //return (*this) * rScale;
+        return Vector2D(x * rScale, y * rScale);
+    }
+
+    __forceinline float DotProduct(const Vector2D& inV)
+    {
+        return x * inV.x + y * inV.y;
+    }
+
+    __forceinline float Length()
+    {
+        return sqrtf(x * x + y * y);
+    }
+
+    __forceinline float SquareLength()
+    {
+        return x * x + y * y;
+    }
+};
+
---
+++ Framework/Math/Vector3D.h
@@ -0,0 +1,102 @@
+#pragma once
+
+#include <math.h>
+
+/**
+ * A vector in 3D with components X, Y, Z   
+ */
+
+struct Vector3D
+{
+public:
+    float x;
+    float y;
+    float z;
+
+    __forceinline Vector3D() {};
+    __forceinline Vector3D(const Vector3D& inV) : x(inV.x), y(inV.y), z(inV.z) {}
+    __forceinline Vector3D(float _v): x(_v), y(_v), z(_v) {}
+    __forceinline Vector3D(float _x, float _y, float _z) : x(_x), y(_y), z(_z) {}
+
+    __forceinline Vector3D& operator= (const Vector3D& inV) 
+    {
+        x = inV.x;
+        y = inV.y;
+        z = inV.z;
+        return *this;
+    }
+
+    __forceinline Vector3D& operator+= (const Vector3D& inV)
+    {
+        x += inV.x;
+        y += inV.y;
+        z += inV.z;
+
+        return *this;
+    }
+
+    __forceinline Vector3D& operator-= (const Vector3D& inV)
+    {
+        x -= inV.x;
+        y -= inV.y;
+        z -= inV.z;
+        return *this;
+    }
+
+    __forceinline Vector3D operator+(const Vector3D& inV)
+    {
+        return Vector3D(x + inV.x, y + inV.y, z + inV.z);
+    }
+
+    __forceinline Vector3D operator- (const Vector3D& inV)
+    {
+        return Vector3D(x - inV.x, y - inV.y, z - inV.z);
+    }
+
+    __forceinline Vector3D operator+(float bias)
+    {
+        return Vector3D(x + bias, y + bias, z + bias);
+    }
+
+    __forceinline Vector3D operator-(float bias)
+    {
+        return Vector3D(x - bias, y - bias, z - bias);
+    }
+
+    __forceinline Vector3D operator*(float scale)
+    {
+        return Vector3D(x * scale, y * scale, z * scale);
+    }
+
+    __forceinline Vector3D operator/(float scale)
+    {
+        const float rScale = 1.f / scale;
+        //return (*this) * rScale;
+        return Vector3D(x * rScale, y * rScale, z * rScale);
+    }
+
+    __forceinline float DotProduct(const Vector3D& inV)
+    {
+        return x * inV.x + y * inV.y + z * inV.z;
+    }
+
+    __forceinline Vector3D CrossProduct(const Vector3D& inV)
+    {
+        return Vector3D(
+            y * inV.z - z * inV.y, 
+            z * inV.x - x * inV.z,
+            x * inV.y - y * inV.x
+            );
+    }
+
+    __forceinline float Length()
+    {
+        return sqrtf(x * x + y * y + z * z);
+    }
+
+    __forceinline float SquareLength()
+    {
+        return x * x + y * y + z * z;
+    }
+};
+
